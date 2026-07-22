f62a131424b4f265f1dcbf56a3474d227a49d8af
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-22 04:55:22 +0100

    Create gbuildet.cxx

---
+++ gbuildet.cxx
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() { 
+printf("Hello World!"); 
+printf("This sentence will work!\n");     // Hello World!This sentence will work!
+printf("Hello World!\n");                // Hello World!
+printf("I am learning C.\n");           // I am learning C.m
+printf("And it is awesome!\n\n");       // And it is awesome!
+// Tab space example
+printf("Name\tAge\tGrade\n");
+printf("Ravi\t20\tA\n");
+printf("Anita\t22\tB\n")
+return 0;
+}
+m
+// Name	Age Grade 
+// Ravi	 20	A 
+// Anita 22	B
